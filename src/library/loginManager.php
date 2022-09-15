86825eac8b58098595396d2be677d95d533edabd
Marta Punset <martapunset10@gmail.com>
2022-09-15 18:28:25 +0200

    json test

--- src/library/loginManager.php
+++ src/library/loginManager.php
@@ -1,11 +1,21 @@
 <?php 
 
 require_once('loginController.php');
+
+
 function validate(){
 
-    #$user = $_POST['email'];
-    #$password = $_POST['password'];
+    echo "json";
+    $user = $_POST['email'];
+    $password = $_POST['password'];
 
-    $user = json_decode('user.json');
-    echo $user["name"];
+    $data = file_get_contents('../../resources/users.json');
+   
+
+    $user = json_decode($data, true);
+   // print_r($user);
+   //var_dump($user);
+    echo ($user["users"][0]["name"]);
+   // print_r($user{"users"}{"userId"});
 }
+?>
