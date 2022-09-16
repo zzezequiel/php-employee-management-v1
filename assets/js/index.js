45730a4915e47ad1d7616bed7adfc036180be08f
zzezequiel <ezequielzvirgzdins@gmail.com>
2022-09-16 14:23:17 +0200

    js fetch testing

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,26 @@
- //import * as data from "../../resources/employees.json";
-//const jsonData = new Request('../../resources/employees.json');
-//console.log(jsonData["name"]);
+
+const employees = [];
+
+let i=0;
 
 
-let data = fetch("../resources/employees.json")
-.then(response => {
-   return response.json();
-})
-.then(jsondata => console.log(jsondata));
+    let data = fetch("../resources/employees.json")
+        .then(response => {
+            return response.json();
+    })
+        .then(jsondata => {
+            for(let e = 0;e<jsondata.length;e++){
+                employees.push(jsondata[e]);
+              
+                
+                console.log(employees[e].name);
+                
+
+
+                
+
+         }
+    });
 
 
 
