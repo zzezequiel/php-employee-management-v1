9e8b102c032cede10df6c68da0a220057dd3c097
Marta Punset <martapunset10@gmail.com>
2022-09-16 20:04:18 +0200

    kk

---
+++ src/library/employeeController.php
@@ -0,0 +1,15 @@
+<?php
+
+    $name=$_POST['name'];
+    $lastName=$_POST['lastName'];
+    $email=$_POST['email'];
+    $gender=$_POST['gender'];
+    $city=$_POST['city'];
+    $street=$_POST['street'];
+    $state=$_POST['state'];
+    $age=$_POST['age'];
+    $postal=$_POST['postal'];
+    $phone=$_POST['phone'];
+    echo $name;
+    echo $lastName;
+?>
