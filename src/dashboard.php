2318f30069f5760be2cdd11fd256b5458d72be68
zzezequiel <ezequielzvirgzdins@gmail.com>
2022-09-21 15:53:57 +0200

    session time

--- src/dashboard.php
+++ src/dashboard.php
@@ -6,10 +6,10 @@
     <meta name="description" content="">
     <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
     <meta name="generator" content="Hugo 0.101.0">
-    <title>Signin Template · Bootstrap v5.2</title>
+    <title>employee manager</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
-    
+    <?php session_start(); ?>
   </head>
   <body>
     <header>
--- src/library/employeeController.php
+++ src/library/employeeController.php
@@ -8,6 +8,18 @@ corresponding functions of it that will be in charge of performing the operation
 /*
 This file will handle the HTTP requests that will be made to create, read, update and delete the employees and for this it must include the  “employeeManager.php” file and call the corresponding functions of it that will be in charge of performing the operations.
 */
+session_start();
+if(!isset($_SESSION['start'])){
+    //Set the session start time
+    $_SESSION['start'] = time();
+}
+if (isset($_SESSION['start']) && (time() - $_SESSION['start'] >600)) {
+   //session_unset();
+   session_destroy();
+
+}
+
+
 include('employeeManager.php');
 
 
