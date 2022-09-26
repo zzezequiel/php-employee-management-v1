63fc79fefaf93e6e73ec75d82d70fb70642006ad
Marta Punset <martapunset10@gmail.com>
2022-09-26 23:15:46 +0200

    delete employee confirmation dialog added

--- assets/js/index.js
+++ assets/js/index.js
@@ -27,6 +27,7 @@ function renderEmployees(employees) {
     for (let e = 0; e < employees.length; e++) {
         let employRow = document.createElement("div");
         let id = employees[e].id;
+       
                 
         employRow.className = "d-flex justify-content-between  mt-2 pb-1";
         containerRows.appendChild(employRow); //add to div claas container1 
@@ -37,13 +38,14 @@ function renderEmployees(employees) {
                     <div class="col text-start ms-5 fs-5">${employees[e].phoneNumber}</div>
                     </div>
 
-                    <div class="col text-end  delete pt-2" id="" type="button" data-bs-toggle="modal" data-bs-target="#modal${employees[e].id}">
+                    <div class="col text-end  delete pt-2" id="del${id}" type="button" data-bs-toggle="modal" data-bs-target="#modal${id}">
                         <svg  xmlns="http://www.w3.org/2000/svg" width="25"  fill="text-danger" class=" bi bi-trash" viewBox="0 0 16 16">
                             <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
                             <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
                         </svg>
                     </div>
-                    <!--MODAL-->                    
+                    <!--MODAL-->     
+                    <script></script>               
                     <div class="modal fade" id="modal${employees[e].id}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
                       <div class="modal-dialog">
                         <div class="modal-content">
@@ -55,60 +57,17 @@ function renderEmployees(employees) {
                           </div>
                           <div class="modal-footer">
                             <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                            <button type="button" onclick="deleteCall(${id})" id="delete${employees[e].id} class="btn btn-primary">Delete</button>
+                            <button type="button" id="delete${id}" data-bs-dismiss="modal" class="btn btn-primary">Delete</button>
                           </div>
                         </div>
                       </div>
                     </div> `;
+        chargeEventListeners(id);
+        employeeDetail(id);
+        
+                     
+    } //closure render for loop
 
-     
-        
-                
-        //get IDs
-     
-        
-        //REdirect Page employee
-        employeeInfo = document.getElementById(`${employees[e].id}`);  //add dinamic id to buttons//add dinamic id to buttons  `${employees[e].id}`
-        let targetEmId = employeeInfo.getAttribute('id');
-      
-
-        employeeInfo.addEventListener("click", function () {
-            location.href = `./library/employeeController.php?action=openEmployee&id=${targetEmId}`;
-            
-
-        })
-        //DELETE
-        
-        
-        // let launchDeleteModal = document.getElementById(`del${employees[e].id}`);
-        //  let targetDelId = launchDeleteModal.getAttribute('id');
-        // targetDelId = targetDelId.substring(3);
-        // launchDeleteModal.addEventListener("click", function () {
-        //   deleteCall(targetDelId, id);
-
-        // })
-        //no se abre el modal porque esta en la funcion, si lo saco de la funcion peta porque no encuentra el eventlisteneer
- 
-        function deleteCall(id) {
-        
-            /*
-            let deleteEmployee = document.getElementById(`delete${id}`);
-            deleteEmployee.addEventListener("click", function () {
-                
-      */
-       
-                console.log("target:" + id);
-                fetch(`./library/employeeController.php?action=delete&id=${id}`)
-                    .then(res => res.json()) ///response.text para vererrores
-                    .then(data => {
-                        console.log(data);
-              
-                        renderEmployees(data);
-                    });
-            
-        }
-                       
-    }
     
     //add employee
     let newEmployee = document.getElementById('newEmployee');
@@ -129,6 +88,37 @@ function renderEmployees(employees) {
     })
 }    
                 
-loadAllEmployeesJS();
 
+ //DELETE
+
+ function chargeEventListeners(id) {  //deleteEventlisteners charge
+    let launchDeleteModal = document.getElementById(`delete${id}`);
+     let targetDelId = launchDeleteModal.getAttribute('id');
+     targetDelId = targetDelId.substring(6);
+     launchDeleteModal.addEventListener("click", function () {
+        
+     deleteCall(targetDelId);
+
+    })
+    }
+
+function deleteCall(id) {
+  
+        fetch(`./library/employeeController.php?action=delete&id=${id}`)
+            .then(res => res.json()) ///response.text para vererrores
+            .then(data => {
+                console.log(data);
+      
+                renderEmployees(data);
+            });
+    
+}
+function employeeDetail(id){
+    employeeInfo = document.getElementById(`${id}`);  //add dinamic id to buttons//add dinamic id to buttons  `${employees[e].id}`
+    let targetEmId = employeeInfo.getAttribute('id');
+    employeeInfo.addEventListener("click", function () {
+        location.href = `./library/employeeController.php?action=openEmployee&id=${targetEmId}`;
+    })
+}
+loadAllEmployeesJS();
 
