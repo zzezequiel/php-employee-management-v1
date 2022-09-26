7e7b56cba55b318adcca04b74ea1228f054cd8a0
Marta Punset <martapunset10@gmail.com>
2022-09-26 23:34:05 +0200

    format

--- src/library/loginController.php
+++ src/library/loginController.php
@@ -7,9 +7,7 @@ if(!empty($_POST['email']) && !empty($_POST['password'])){
     
     validate();
 }
-else{
-    #header("location:../../index.php"); brings out problems with local host
-}
+
 
 if(isset($_GET['action'] )&& $_GET['action']=="logout"){
 logOut();
